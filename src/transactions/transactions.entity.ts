00d983c29bb24bf7b8f05bad270f60f3552223d6
Tiago Almeida <tm.almeida@ua.pt>
2019-09-05 19:50:13 +0100

    transactions

---
+++ src/transactions/transactions.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+
+@Entity({ name: 'Transactions' })
+export class Transactions {
+  @PrimaryGeneratedColumn({ name: 'id' })
+  productId?: number;
+
+  @Column({ length: 25 })
+  name: string;
+
+  @CreateDateColumn()
+  createdAt?: string;
+
+  @Column('point')
+  locatedIn: number;
+
+  @Column('string')
+  type: string;
+
+}
